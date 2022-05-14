bc4475ceb9f3a06bc0419363d72800c9b843e7d1
Diego <diego.pompeo@milvus.com.br>
2022-05-14 18:07:53 -0300

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AuthGuard } from './core/auth/auth.guard';
 import { CadastroComponent } from './pages/cadastro/cadastro.component';
 import { LoginComponent } from './pages/login/login.component';
 import { PaginaInicialComponent } from './pages/pagina-inicial/pagina-inicial.component';
@@ -8,7 +9,7 @@ const routes: Routes = [
   { path: "login", component: LoginComponent },
   { path: "cadastro", component: CadastroComponent },
 
-  { path: "", component: PaginaInicialComponent },
+  { path: "", component: PaginaInicialComponent, canActivate: [AuthGuard] },
 
   { path: '**', redirectTo: '' }
 ];
---
+++ src/app/core/auth/auth.guard.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { AuthService } from 'src/app/services/auth/auth.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard implements CanActivate {
+
+  constructor(private router: Router, private authService: AuthService) {
+    
+  }
+
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    let isAuthenticated = this.authService.isAuthenticated();
+    if (isAuthenticated) {
+      return true
+    } else {
+      this.router.navigate(['/login'], { queryParams: { returnUrl: state.url } })
+      return false;
+    }
+  }
+  
+}
